3d54b98346a4764fe0d2c6fa1757854aebbb6507
Sarang Baheti <sarangbaheti@gmail.com>
2017-09-07 22:27:16 -0700

    got most of it working now

--- cpustress/MainWindow.xaml.cs
+++ cpustress/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Windows;
 using MahApps.Metro.Controls;
+using System.Timers;
+using System.ComponentModel;
 
 namespace cpustress
 {
@@ -12,12 +14,57 @@ namespace cpustress
     public partial class MainWindow : MetroWindow
     {
         private List<ThreadPool_WorkerFunction> _tasks = new List<ThreadPool_WorkerFunction>();
+        private CPUUsage CPU = new CPUUsage();
+        private Timer _timer = new Timer();
 
         public MainWindow()
         {
             InitializeComponent();
             sliderCPU.Maximum = SystemInfo.GetNumProcessors();
             this.Title = string.Format("{0} ({1})", this.Title, SystemInfo.GetComputerName());
+
+            _timer.Enabled = true;
+            _timer.Interval = 250;
+            _timer.Elapsed += _timer_Elapsed;
+            _timer.Start();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            _timer.Stop();
+            _timer.Dispose();
+        }
+
+        internal void StopTimerOnShutdown()
+        {
+            _timer.Enabled = false;
+            _timer.Stop();
+        }
+
+
+        private static double Clamp(double val, double min, double max)
+        {
+            if (val < min)
+                return min;
+            else if (val > max)
+                return max;
+            return val;
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            lock (this)
+            {
+                CPU.Update();
+                this.Dispatcher.Invoke(() =>
+                {
+                    var cpuVal = Clamp(CPU.CPU, 0.0, 100.0);
+                    _cpuDial.Value = cpuVal;
+
+                    _cpuLabel.Content = string.Format("CPU {0} %", cpuVal.ToString("N2"));
+                });
+            }
         }
 
         private void sliderCPU_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
